336d1fc4bf14a4fa7424c5501ea61f70fb2d4a31
Anders G. Jørgensen <spirit55555@gmail.com>
2021-10-23 01:31:35 +0200

    Fix some formatting in tests

--- tests/MinecraftColorsTest.php
+++ tests/MinecraftColorsTest.php
@@ -24,15 +24,15 @@ use \Spirit55555\Minecraft\MinecraftColors;
 final class MinecraftColorsTest extends TestCase {
 	public function testClean(): void {
 		$text = '§4Lorem §3§lipsum §rdolor &nsit &c&mamet';
-		$text_hex = "§#AA0000Lorem §3§lipsum §rdolor &nsit &#FF5555&kamet";
+		$text_hex = '§#AA0000Lorem §3§lipsum §rdolor &nsit &#FF5555&kamet';
 
 		$this->assertSame('Lorem ipsum dolor sit amet', MinecraftColors::clean($text));
 		$this->assertSame('Lorem ipsum dolor sit amet', MinecraftColors::clean($text_hex));
 	}
 
 	public function testConvertToHTML(): void {
-		$text = "§4Lorem §3§lipsum\n§rdolor &nsit &c&kamet";
-		$text_hex = "§#AA0000Lorem §3§lipsum\n§rdolor &nsit &#FF5555&kamet";
+		$text = '§4Lorem §3§lipsum\n§rdolor &nsit &c&kamet';
+		$text_hex = '§#AA0000Lorem §3§lipsum\n§rdolor &nsit &#FF5555&kamet';
 
 		$this->assertSame('<span style="color: #AA0000">Lorem </span><span style="color: #00AAAA"><span style="font-weight: bold;">ipsum</span></span>'."\n".'dolor <span style="text-decoration: underline;">sit </span><span style="color: #FF5555"><span class="minecraft-formatted--obfuscated">amet</span></span>', MinecraftColors::convertToHTML($text));
 		$this->assertSame('<span style="color: #AA0000">Lorem </span><span style="color: #00AAAA"><span style="font-weight: bold;">ipsum</span></span><br />dolor <span style="text-decoration: underline;">sit </span><span style="color: #FF5555"><span class="minecraft-formatted--obfuscated">amet</span></span>', MinecraftColors::convertToHTML($text, true));
@@ -52,7 +52,7 @@ final class MinecraftColorsTest extends TestCase {
 
 	public function testConvertToMOTD(): void {
 		$text = '§4Lorem §3§lipsum §rdolor &nsit &c&mamet';
-		$text_hex = "§#aa0000Lorem §3§lipsum §rdolor &nsit &#ff5555&mamet";
+		$text_hex = '§#aa0000Lorem §3§lipsum §rdolor &nsit &#ff5555&mamet';
 
 		$this->assertSame('\u00A74Lorem \u00A73\u00A7lipsum \u00A7rdolor \u00A7nsit \u00A7c\u00A7mamet', MinecraftColors::convertToMOTD($text));
 		$this->assertSame('&4Lorem &3&lipsum &rdolor &nsit &c&mamet', MinecraftColors::convertToMOTD($text, '&'));
